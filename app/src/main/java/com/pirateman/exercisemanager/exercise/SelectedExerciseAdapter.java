d7b6336e4ef566ef8943bd37512d10cdb8ea455c
pmessina <pmm0501@gmail.com>
2018-10-26 19:28:48 -0400

    Adapter for selected exercise recyclerview

--- app/src/main/java/com/pirateman/exercisemanager/exercise/SelectedExerciseAdapter.java
+++ app/src/main/java/com/pirateman/exercisemanager/exercise/SelectedExerciseAdapter.java
@@ -1,14 +1,12 @@
 package com.pirateman.exercisemanager.exercise;
 
 import android.content.Context;
-import android.databinding.DataBindingUtil;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.pirateman.exercisemanager.R;
-import com.pirateman.exercisemanager.databinding.RecyclerViewExerciseItemBindingImpl;
-import com.pirateman.exercisemanager.databinding.RecyclerViewSelectedExerciseItemBindingImpl;
+import com.pirateman.exercisemanager.databinding.SelectedExerciseItemBinding;
+
+import androidx.annotation.NonNull;
 
 public class SelectedExerciseAdapter extends ExerciseAdapter
 {
@@ -21,19 +19,19 @@ public class SelectedExerciseAdapter extends ExerciseAdapter
     @Override
     public BaseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        RecyclerViewSelectedExerciseItemBindingImpl binding = DataBindingUtil.inflate(inflater, R.layout.recycler_view_selected_exercise_item, parent, false);
+        SelectedExerciseItemBinding binding = SelectedExerciseItemBinding.inflate(inflater, parent, false);
 
         return new SelectedExerciseHolder(context, binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BaseHolder holder, int position) {
-        Exercise item = exerciseList.get(position);
+        Exercise item = getExerciseList().get(position);
         holder.bind(item);
     }
 
     @Override
     public int getItemCount() {
-        return exerciseList.size();
+        return getExerciseList().size();
     }
 }
