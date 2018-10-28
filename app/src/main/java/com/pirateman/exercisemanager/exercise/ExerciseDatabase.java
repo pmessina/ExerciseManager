9eb0950d47b39c2dd5d7364c0784150c99f531ba
pmessina <pmm0501@gmail.com>
2018-10-28 16:04:44 -0400

    remove unused imports

--- app/src/main/java/com/pirateman/exercisemanager/exercise/ExerciseDatabase.java
+++ app/src/main/java/com/pirateman/exercisemanager/exercise/ExerciseDatabase.java
@@ -1,15 +1,9 @@
 package com.pirateman.exercisemanager.exercise;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.Database;
-import android.arch.persistence.room.DatabaseConfiguration;
-import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
-import android.support.annotation.NonNull;
 
 @Database(entities = {Exercise.class, Interval.class}, version = 3, exportSchema = false)
 public abstract class ExerciseDatabase extends RoomDatabase
