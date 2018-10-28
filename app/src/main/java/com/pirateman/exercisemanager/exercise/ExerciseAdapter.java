3501331859f67ac82f0ddc79d64a0d8af961a7ff
pmessina <pmm0501@gmail.com>
2018-10-28 15:34:03 -0400

    add getter for exercise adapter

--- app/src/main/java/com/pirateman/exercisemanager/exercise/ExerciseAdapter.java
+++ app/src/main/java/com/pirateman/exercisemanager/exercise/ExerciseAdapter.java
@@ -1,37 +1,44 @@
 package com.pirateman.exercisemanager.exercise;
 
 import android.content.Context;
-import android.databinding.DataBindingUtil;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.pirateman.exercisemanager.R;
-import com.pirateman.exercisemanager.databinding.RecyclerViewExerciseItemBindingImpl;
+import com.pirateman.exercisemanager.databinding.ExerciseItemBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExerciseAdapter extends RecyclerView.Adapter<BaseHolder>{
+public class ExerciseAdapter extends RecyclerView.Adapter<BaseHolder>
+{
 
-    protected List<Exercise> exerciseList;
+    private List<Exercise> exerciseList;
 
     protected Context context;
 
-    public ExerciseAdapter(Context context) {
+    public ExerciseAdapter(Context context)
+    {
         exerciseList = new ArrayList<>();
         this.context = context;
     }
 
-    public ExerciseAdapter(Context context, @LayoutRes int layoutId) {
+    public ExerciseAdapter(Context context, @LayoutRes int layoutId)
+    {
         exerciseList = new ArrayList<>();
         this.context = context;
     }
 
-    public ExerciseAdapter(List<Exercise> exerciseList) {
+    public ExerciseAdapter(ArrayList<Exercise> exerciseList)
+    {
         this.exerciseList = exerciseList;
+    }
+
+    public List<Exercise> getExerciseList()
+    {
+        return exerciseList;
     }
 
     public void setExercise(Exercise exercise)
@@ -59,21 +66,24 @@ public class ExerciseAdapter extends RecyclerView.Adapter<BaseHolder>{
 
     @NonNull
     @Override
-    public BaseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public BaseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        RecyclerViewExerciseItemBindingImpl binding = DataBindingUtil.inflate(inflater, R.layout.recycler_view_exercise_item, parent, false);
+        ExerciseItemBinding binding = ExerciseItemBinding.inflate(inflater, parent, false);
 
         return new ExerciseHolder(context, binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BaseHolder holder, int position) {
+    public void onBindViewHolder(@NonNull BaseHolder holder, int position)
+    {
         Exercise item = exerciseList.get(position);
         holder.bind(item);
     }
 
     @Override
-    public int getItemCount() {
+    public int getItemCount()
+    {
         return exerciseList.size();
     }
 
